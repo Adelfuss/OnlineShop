ec698a8eb548f2851887675a5546298c350ab95d
bohdan_lyhoshapko <bohdan09012000@gmail.com>
2021-03-30 20:56:08 +0300

    Add html templates in project

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -13,19 +13,7 @@ class MainController extends AppController
 
     public function indexAction()
     {
-        $posts = \R::findAll('test');
-        $post = \R::findOne('test','id = ?', [2]);
         $this->setMeta(App::$app->getProperty('shop_name'), 'Описание...', 'Ключевики...');
-        $name = 'John';
-        $age = 30;
-        $names = ['Bohdan', 'Lyhoshapko','Mike'];
-        $cache = Cache::instance();
-        $data = $cache->get('test');
-        if (!$data) {
-            $cache->set('test',$names);
-        }
-        debug($data);
-        $this->set(compact('name','age','names','posts'));
     }
 
 }
