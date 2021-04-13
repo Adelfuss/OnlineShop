13c17b59bffa00e2bb17869c318c504579596024
bohdan_lyhoshapko <bohdan09012000@gmail.com>
2021-04-13 23:30:58 +0300

    Recently viewed goods

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -4,6 +4,8 @@
 namespace app\controllers;
 
 
+use app\models\Product;
+
 class ProductController extends AppController
 {
     public function viewAction()
@@ -18,14 +20,20 @@ class ProductController extends AppController
         // связанные товары
         $related = \R::getAll("SELECT * FROM related_product JOIN product ON product.id = related_product.related_id WHERE related_product.product_id = ?", [$product->id]);
         //записать в куки запрошенный товар
+        $p_model = new Product();
+        $p_model->setRecentlyViewed($product->id);
 
         // просмотренные товари
-
+        $r_viewed = $p_model->getRecentlyViewed();
+        $recentlyViewed = null;
+        if ($r_viewed) {
+            $recentlyViewed = \R::find('product','id IN (' .\R::genSlots($r_viewed). ') LIMIT 3', $r_viewed);
+        }
         // галерея
         $galery = \R::findAll('gallery','product_id = ?',[$product->id]);
         // модификации
 
         $this->setMeta($product->title, $product->description,$product->keywords);
-        $this->set(compact('product','related','galery'));
+        $this->set(compact('product','related','galery','recentlyViewed'));
     }
 }
